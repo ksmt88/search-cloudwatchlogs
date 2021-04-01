69b8dd6f5a84f5781e5ad4d53c70cb963e224f21
ksmt88 <sakonmll@gmail.com>
2021-04-01 22:40:45 +0900

    Add go mod

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+    "flag"
+
     "github.com/ksmt88/search-cloudwatchlogs/cmd"
 )
 
 func main() {
+    profile := flag.String("profile", "", "profile name")
+    flag.Parse()
+
     input := cmd.Input{
-        Profile: "",
+        Profile: *profile,
     }
     cmd.Search(&input)
 }
